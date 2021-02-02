23adda96902d579505ddd346edae656b0adff32d
DESKTOP-6AUVDGQ\咸鱼 <2669531054@qq.com>
2021-02-02 23:36:18 +0800

    no message

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -35,10 +35,10 @@ let fruitSlowDown = false;
 let clickChangeFruit = false;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
+let adLink = false;
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
+document.getElementsByTagName("title")[0].innerText = '合成大肖欣雨';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
