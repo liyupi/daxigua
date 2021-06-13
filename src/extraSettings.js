6b873c39fa74d11103142ba23e9bcb1433825419
Mikoto <26019675+lwd-temp@users.noreply.github.com>
2021-06-13 22:21:56 +0800

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -6,10 +6,10 @@
 let extraScore = 1;
 
 // 无敌模式：true 改为 false
-let wuDi = true;
+let wuDi = false;
 
 // 第一个水果：修改数字为 0-10, 0 为葡萄，9 为半个西瓜
-let firstFruit = 0;
+let firstFruit = 9;
 
 // 水果合成反转：false 改为 true
 let reverseLevelUp = false;
@@ -26,20 +26,20 @@ let setFruits = {
 }
 
 // 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
-let fruitQTan = false;
+let fruitQTan = 0.9;
 
 // 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
-let fruitSlowDown = false;
+let fruitSlowDown = 5;
 
 // 点击右上方图标更换水果：false 改为 true 即可
 let clickChangeFruit = true;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
+let adLink = false;
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
+document.getElementsByTagName("title")[0].innerText = '合成大西瓜（改）';
 
 // 开启选分弹窗：将 false 改为 true
-let selectModal = false;
+let selectModal = true;
 
